71221c3744b86610894a64b8cfcb8058c8b7fddc
unknown <calebjanhunen@gmail.com>
2022-06-07 22:09:40 -0400

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("hey");
+});
+
+mongoose
+    .connect(process.env.CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(process.env.PORT, () =>
+            console.log(`Server is up on port: ${process.env.PORT}`)
+        )
+    )
+    .catch(err => console.log(err));
