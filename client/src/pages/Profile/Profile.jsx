0a9b9ce207df60c28f6921127fd317ed934341e8
Caleb Janhunen <calebjanhunen@gmail.com>
2022-08-10 14:24:42 -0400

    Added some profile page content

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Avatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import './Profile.css';
 
 const Profile = () => {
-    return <div>Profile</div>;
+    const username = useSelector(state => state.auth.user);
+    const [avatarDisplay, setAvatarDisplay] = useState(username[0]);
+    const [avatarHover, setAvatarHover] = useState(false);
+    return (
+        <div>
+            <Avatar
+                id="profile-avatar"
+                onMouseEnter={() => setAvatarHover(true)}
+                onMouseLeave={() => setAvatarHover(false)}
+            >
+                {avatarHover ? (
+                    <p className="change-profile-pic">Change Profile Picture</p>
+                ) : (
+                    avatarDisplay
+                )}
+            </Avatar>
+            <p>Username: {username}</p>
+        </div>
+    );
 };
 
 export default Profile;
